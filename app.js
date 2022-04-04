fd887997629c65df180ffbe8e48a6c521d54f34b
AJAY YADAV <aju9617@gmail.com>
2022-04-04 09:10:04 +0530

    Initial commit 🚀

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.listen(process.env.PORT, () => {
+  console.log(`server is up and running on PORT ${process.env.PORT}`);
+});
+
+module.exports = app;
